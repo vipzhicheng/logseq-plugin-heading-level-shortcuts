d7807f4dcfbb6e9bb54b45d29a924d8aa7245198
vipzhicheng <vipzhicheng@gmail.com>
2022-03-12 22:08:12 +0800

    feat: support multiple blocks selections shortcuts

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,60 @@
 import '@logseq/libs';
 
 const repeat = (str: string, times: number) => {
-  return [...Array(times).keys()].reduce(prev => { return prev + str; }, '');
+  return [...Array(times).keys()].reduce(prev => {
+    return prev + str;
+  }, '');
 };
 
 async function setLevel(level: number) {
-  const block = await logseq.Editor.getCurrentBlock();
-    if (block?.uuid) {
-      let content = /^#{1,6}\s+/.test(block.content) ? block.content.replace(/^#{1,6}\s+/, '') : block.content;
+  const selected = await logseq.Editor.getSelectedBlocks();
+  if (selected && selected.length > 1) {
+    for (let block of selected) {
+      let content = /^#{1,6}\s+/.test(block.content)
+        ? block.content.replace(/^#{1,6}\s+/, '')
+        : block.content;
       if (level > 0) {
-        await logseq.Editor.updateBlock(block.uuid, repeat('#', level) + ' ' + content);
+        await logseq.Editor.updateBlock(
+          block.uuid,
+          repeat('#', level) + ' ' + content
+        );
       } else {
         await logseq.Editor.updateBlock(block.uuid, content);
       }
     }
+  } else {
+    const block = await logseq.Editor.getCurrentBlock();
+    if (block?.uuid) {
+      let content = /^#{1,6}\s+/.test(block.content)
+        ? block.content.replace(/^#{1,6}\s+/, '')
+        : block.content;
+      if (level > 0) {
+        await logseq.Editor.updateBlock(
+          block.uuid,
+          repeat('#', level) + ' ' + content
+        );
+      } else {
+        await logseq.Editor.updateBlock(block.uuid, content);
+      }
+    }
+  }
 }
 
 async function main() {
   for (let level of [0, 1, 2, 3, 4, 5, 6]) {
-    logseq.App.registerCommandPalette({
-      key: `heading-level-shortcuts-h${level}`,
-      label: `Set block to heading level ${level}`,
-      keybinding: {
-        mode: 'global',
-        binding: 'mod+' + level
+    logseq.App.registerCommandPalette(
+      {
+        key: `heading-level-shortcuts-h${level}`,
+        label: `Set block to heading level ${level}`,
+        keybinding: {
+          mode: 'global',
+          binding: 'mod+' + level,
+        },
+      },
+      async () => {
+        await setLevel(level);
       }
-    }, async () => {
-      await setLevel(level);
-    });
+    );
   }
 }
 
