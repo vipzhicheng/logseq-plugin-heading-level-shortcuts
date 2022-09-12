9796569262c5c46092010c36908739f76b4c77d7
Richard Yu <vipzhicheng@gmail.com>
2022-09-12 11:58:12 +0800

    feat: add settings file support.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # CHANGELOG
 
+## v0.0.3
+
+- feat: add settings file support.
+- infra: upgrade deps.
+
 ## v0.0.2
 
 - feat: support multiple blocks selections shortcuts
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,43 @@
 import '@logseq/libs';
 
+const settingsVersion = 'v1';
+export const defaultSettings = {
+  keyBindings: {
+    1: 'mod+1',
+    2: 'mod+2',
+    3: 'mod+3',
+    4: 'mod+4',
+    5: 'mod+5',
+    6: 'mod+6',
+    0: 'mod+0',
+  },
+  settingsVersion,
+  disabled: false,
+};
+
+export type DefaultSettingsType = typeof defaultSettings;
+
+const initSettings = () => {
+  let settings = logseq.settings;
+
+  const shouldUpdateSettings =
+    !settings || settings.settingsVersion != defaultSettings.settingsVersion;
+
+  if (shouldUpdateSettings) {
+    settings = defaultSettings;
+    logseq.updateSettings(settings);
+  }
+};
+
+const getSettings = (
+  key: string | undefined,
+  defaultValue: any = undefined
+) => {
+  let settings = logseq.settings;
+  const merged = Object.assign(defaultSettings, settings);
+  return key ? (merged[key] ? merged[key] : defaultValue) : merged;
+};
+
 const repeat = (str: string, times: number) => {
   return [...Array(times).keys()].reduce(prev => {
     return prev + str;
@@ -7,16 +45,24 @@ const repeat = (str: string, times: number) => {
 };
 
 async function setLevel(level: number) {
+  let regex = /^#{1,6}\s+/;
+  let headingSign = '#';
+  // const config = await logseq.App.getUserConfigs();
+  // if (config.preferredFormat === 'org') {
+  //   regex = /^\*{1,6}\s+/;
+  //   headingSign = '*';
+  // }
+
   const selected = await logseq.Editor.getSelectedBlocks();
   if (selected && selected.length > 1) {
     for (let block of selected) {
-      let content = /^#{1,6}\s+/.test(block.content)
-        ? block.content.replace(/^#{1,6}\s+/, '')
+      let content = regex.test(block.content)
+        ? block.content.replace(regex, '')
         : block.content;
       if (level > 0) {
         await logseq.Editor.updateBlock(
           block.uuid,
-          repeat('#', level) + ' ' + content
+          repeat(headingSign, level) + ' ' + content
         );
       } else {
         await logseq.Editor.updateBlock(block.uuid, content);
@@ -25,13 +71,13 @@ async function setLevel(level: number) {
   } else {
     const block = await logseq.Editor.getCurrentBlock();
     if (block?.uuid) {
-      let content = /^#{1,6}\s+/.test(block.content)
-        ? block.content.replace(/^#{1,6}\s+/, '')
+      let content = regex.test(block.content)
+        ? block.content.replace(regex, '')
         : block.content;
       if (level > 0) {
         await logseq.Editor.updateBlock(
           block.uuid,
-          repeat('#', level) + ' ' + content
+          repeat(headingSign, level) + ' ' + content
         );
       } else {
         await logseq.Editor.updateBlock(block.uuid, content);
@@ -41,6 +87,9 @@ async function setLevel(level: number) {
 }
 
 async function main() {
+  initSettings();
+  const keyBindings = getSettings('keyBindings', {});
+
   for (let level of [0, 1, 2, 3, 4, 5, 6]) {
     logseq.App.registerCommandPalette(
       {
@@ -48,7 +97,7 @@ async function main() {
         label: `Set block to heading level ${level}`,
         keybinding: {
           mode: 'global',
-          binding: 'mod+' + level,
+          binding: keyBindings[level] || 'mod+' + level,
         },
       },
       async () => {
